faa67bc6dcefe08b182e14454f55590e60faa77d
Dewey Ou <oushihao97@gmail.com>
2023-01-22 16:14:30 +0800

    chore: initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default observer(() => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="text-3xl pb-10">HelloWorld</div>
+
+    </div>
+  )
+})
