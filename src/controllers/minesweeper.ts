9d4c9d9e6231aa1e7c2ba2fcacfb25b5a600ec99
Dewey Ou <oushihao97@gmail.com>
2023-01-29 00:59:23 +0800

    fix: set initial when right click flag

--- src/controllers/minesweeper.ts
+++ src/controllers/minesweeper.ts
@@ -121,7 +121,7 @@ export class MinesweeperController {
   private _handleRightButtonClick(block: MinesweeperItemState) {
     if (block.type === MINESWEEPER_ITEM_TYPE.INITIAL)
       block.type = MINESWEEPER_ITEM_TYPE.FLAG
-    if (block.type === MINESWEEPER_ITEM_TYPE.FLAG)
+    else if (block.type === MINESWEEPER_ITEM_TYPE.FLAG)
       block.type = MINESWEEPER_ITEM_TYPE.INITIAL
   }
 
